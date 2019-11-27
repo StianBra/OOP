680adbe5e6073befc33d28c5ea5cc956f5d5a34a
Stian Bratlie <stiabra@stud.ntnu.no>
2019-11-27 15:07:00 +0100

    Laget amountOfNeighbours-funksjon for å fullføre testEmptyBoard

--- Java/IMT3281/Eksamensøving/S2018/src/main/java/no/ntnu/imt3281/S2018/GameOfLife.java
+++ Java/IMT3281/Eksamensøving/S2018/src/main/java/no/ntnu/imt3281/S2018/GameOfLife.java
@@ -17,10 +17,37 @@ public class GameOfLife {
     public GameOfLife(int rows, int columns) {
         grid = new boolean[rows][columns];
 
+        // Sets all cells on the grid to false (dead)
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
                 grid[i][j] = false;
             }
         }
+    }
+
+    /**
+     * Checks how many neighbours a given cell [row, column] has
+     * (the amount of cells in a 3x3 grid centered around the given [row, column] cell that are true)
+     * @param row The row for the cell to check
+     * @param column The column for the cell to check
+     * @return 0 if no neighbours, 1-8 if some neighbours
+     */
+    public int amountOfNeighbours(int row, int column) {
+        // The amount of neighbours found
+        int neighbours = 0;
+
+        // Checks all cells in a 3x3 grid centered around the given point [row, column]
+        for (int i = row - 1; i <= row + 2; i++) {
+            for (int j = column - 1; j <= column + 2; j++) {
+                // Takes care not to count the cell itself
+                if ((i != row || j != column) && grid[i][j]) {
+                    // If the cell is alive, then increase the amount of neighbours
+                    neighbours++;
+                }
+            }
+        }
+
+        // Returns the amount of neighbours
+        return neighbours;
     }
 }
--- Java/IMT3281/Eksamensøving/S2018/src/main/test/no/ntnu/imt3281/S2018/GameOfLifeTest.java
+++ Java/IMT3281/Eksamensøving/S2018/src/main/test/no/ntnu/imt3281/S2018/GameOfLifeTest.java
@@ -5,8 +5,8 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameOfLifeTest {
-    public static int ROWSIZE = 4;
-    public static int COLUMNSIZE = 8;
+    private static final int ROWSIZE = 4;
+    private static final int COLUMNSIZE = 8;
 
     /**
      * Creates an empty board, and checks that [1, 4] has no neighbours

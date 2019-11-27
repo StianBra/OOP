efcbbd0df69ae5a9fa9f265a976a9a5b6d0f9930
Stian Bratlie <stiabra@stud.ntnu.no>
2019-11-27 14:52:33 +0100

    testEmptyBoard laget

--- Java/IMT3281/Eksamensøving/S2018/src/main/test/no/ntnu/imt3281/S2018/GameOfLifeTest.java
+++ Java/IMT3281/Eksamensøving/S2018/src/main/test/no/ntnu/imt3281/S2018/GameOfLifeTest.java
@@ -1,4 +1,22 @@
 package no.ntnu.imt3281.S2018;
 
-public class GameOfLifeTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameOfLifeTest {
+    public static int ROWSIZE = 4;
+    public static int COLUMNSIZE = 8;
+
+    /**
+     * Creates an empty board, and checks that [1, 4] has no neighbours
+     */
+    @Test
+    void testEmptyBoard() {
+        // Creates an empty board
+        GameOfLife game = new GameOfLife(ROWSIZE, COLUMNSIZE);
+
+        // Checks that [1, 4] has 0 neighbours
+        assertEquals(0, game.amountOfNeighbours(1, 4));
+    }
 }

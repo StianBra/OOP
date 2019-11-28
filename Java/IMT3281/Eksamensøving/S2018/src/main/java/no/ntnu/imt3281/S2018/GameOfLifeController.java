019db32e556b77115a56b0d601e2c0be918f5a47
Stian Bratlie <stiabra@stud.ntnu.no>
2019-11-28 10:06:49 +0100

    Fikset oppgave 4, kan nå markere celler som levende/døde i GUI

--- Java/IMT3281/Eksamensøving/S2018/src/main/java/no/ntnu/imt3281/S2018/GameOfLifeController.java
+++ Java/IMT3281/Eksamensøving/S2018/src/main/java/no/ntnu/imt3281/S2018/GameOfLifeController.java
@@ -10,7 +10,7 @@ import javafx.scene.shape.Rectangle;
 import java.util.Optional;
 
 /**
- *
+ * Controller-class for the game of life GUI
  */
 public class GameOfLifeController {
 
@@ -27,7 +27,12 @@ public class GameOfLifeController {
     private int rows = 10;
     private int columns = 10;
     private Rectangle[][] rectangles;
+    private double widthPerGrid;
+    private double heightPerGrid;
 
+    /**
+     * Initializes some of the GUI elements to sane defaults
+     */
     @FXML
     private void initialize() {
         rowField.setText(String.valueOf(rows));
@@ -36,10 +41,13 @@ public class GameOfLifeController {
         rectangles = new Rectangle[rows][columns];
     }
 
+    /**
+     * Draws all the rectangles for the game of life, given by amount of rows/columns
+     */
     @FXML
     void drawGrid() {
-        double widthPerGrid = gameOfLife.getWidth() / rows;
-        double heightPerGrid = gameOfLife.getHeight() / columns;
+        widthPerGrid = gameOfLife.getWidth() / rows;
+        heightPerGrid = gameOfLife.getHeight() / columns;
 
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
@@ -72,7 +80,29 @@ public class GameOfLifeController {
         }
     }
 
+    /**
+     * Sets a living (black) cell to a dead (white) cell, and vice verca
+     * @param mouseEvent The registered click on a cell
+     */
     public void updateCell(MouseEvent mouseEvent) {
+        // First finds the x/y coordinates for the source, divided by per-grid size to get the array-indexes
+        // Also, uses getX and getY in order to get Y and X respectively, due to arrays/GUI coordinates being opposite
+        int y = (int) (mouseEvent.getX() / widthPerGrid);
+        int x = (int) (mouseEvent.getY() / heightPerGrid);
 
+        // If the rectangle we have located was white
+        if (rectangles[x][y].getFill() == Color.TRANSPARENT) {
+            // Then we will register it as a living cell in the game of life
+            game.setLivingCell(x, y);
+
+            // And color it black
+            rectangles[x][y].setFill(Color.BLACK);
+        } else {
+            // The rectangle was black, so we will register it as a dead cell in the game of life
+            game.setDeadCell(x, y);
+
+            // And color it white
+            rectangles[x][y].setFill(Color.TRANSPARENT);
+        }
     }
 }
